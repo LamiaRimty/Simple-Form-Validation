e911a7cb60160724756a76c94bf1b641e8e828ff
LamiaRimty <rimtycse@gmail.com>
2018-03-20 07:45:57 +0600

    first creation

--- index.php
+++ index.php
@@ -30,6 +30,16 @@
 		<input type="text" name="email"  id="email"/>
 		</p>
 
+<div class="well"> 
+      <div class="form-group">
+      <label>Date of Birth</label>
+      <input type="date" class="form-control" id="exampleInputDOB1" placeholder="Date of Birth">
+    </div>
+</div>
+
+<div class="well"> 
+</div>
+
 
 		<p>
 		<label>Passsword</label>
@@ -37,6 +47,30 @@
 		
 		<input type="submit" name="Submit">
 	</p>
+
+<input id="numb">
+
+<button type="button" onclick="myFunction()">Submit</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+    var x, text;
+
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("numb").value;
+
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1 || x > 10) {
+        text = "Input not valid";
+    } else {
+        text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
+</script>
+
 
 	</form>
 	</body>
